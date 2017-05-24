74174d7ca80aa5eba162f8356aacf27b7e81d252
Keith Chen <Keith.Chen@quest.com>
2017-05-24 02:06:58 -0700

    add some code for test case table populator, and add testcase name field to TestCase object

--- src/populators/testcaseTablePopulator.ts
+++ src/populators/testcaseTablePopulator.ts
@@ -1,10 +1,63 @@
 import {TestSuite} from '../robotModels/TestSuite';
 import {TestCase} from '../robotModels/TestCase';
+import {Variable} from '../robotModels/Variable';
 
 export class TestCaseTablePopulator {
+     // feed document's test case table to suite, the content of document is begin with startLine
+     // return the next line to search in the document
+     // this static method need unit test
+     static populate(lineContentList : string[], startLine : number, suite : TestSuite) : number
+     {
+         let currentLineNumber : number = startLine;
+         let lineCount : number = lineContentList.length;
+         let endPattern = new RegExp("\*\*\*(.*)\*\*\*");
+         // match for the test case name
+         let testCaseNamePattern = /^(\S+ )+.*/i;
 
-}
+         while (currentLineNumber < lineCount) {
+             let currentLine : string = lineContentList[currentLineNumber];
 
-class LocalVariablePopulator {
+             if (currentLine.match(endPattern)) {
+                 return currentLineNumber;
+             } else if (currentLine.match(testCaseNamePattern)) {
+                 let tc : TestCase = new TestCase();
+                 currentLineNumber = TestCaseTablePopulator.populateLocalVarToTestCase(lineContentList, currentLineNumber, tc);
+                 suite.testCases.push(tc);                 
+             } else {
+                 // may meed empty line between test case
+                 ++currentLineNumber
+             }
+         }
+     }
 
+     // feed documents local variables, startline and endline to TestCase
+     // return the next line to search in the document
+     // and it should only be used by TestCaseTablePopulator.populate
+     // the startLine is the first line of TestCase body(with test case name)
+     static populateLocalVarToTestCase(lineContentList : string[], startLine : number, testCase : TestCase) : number 
+     {
+          let currentLineNumber : number = startLine;  
+          let lineCount : number = lineContentList.length;
+
+          testCase.startLine = currentLineNumber;
+          testCase.testname = lineContentList[currentLineNumber];
+          ++currentLineNumber;
+
+          // match for function return value in test case table like
+          //     ${variable} =     this is a func    ${arg1}
+          let retVarPattern = /^(\t{1,}|\s{2,})\$\{(.+)\} = (\S+ )*(\S+ )/i;
+          // match for body in test case
+          // it's started with 2 or spaces or 1 or more tabs
+          //     this is a keyword    ${arg}
+          let testCaseBodyPattern = /^(\t{1,}|\s{2,}).*/i;
+
+          while (lineContentList[currentLineNumber].match(testCaseBodyPattern)) {
+              let varAssignMatch = lineContentList[currentLineNumber].match(retVarPattern);
+              if (varAssignMatch) {
+                  let v : Variable = new Variable(currentLineNumber, varAssignMatch[2]);
+              }
+              ++currentLineNumber;
+          }
+
+     }
 }
--- src/robotModels/TestCase.ts
+++ src/robotModels/TestCase.ts
@@ -7,9 +7,15 @@ import {Variable} from './Variable'
  * statements, it just used for goto variable definition
  */
 export class TestCase {
+    /**
+     * Note for the startLine and endLine
+     * it represent the TestCase body line range in TestSuite
+     * and the range is include startLine, not include endLine, in math, it's [startLine, endLine)
+     */
     private _startLine : Number;
     private _endLine : Number;
     private _variables : Set<Variable>;
+    private _testname : string;
 
     public get startLine() : Number { return this._startLine; }
     public set startLine(value : Number) { this._startLine = value; }
@@ -20,6 +26,9 @@ export class TestCase {
     public get variables() : Set<Variable> { return this._variables; }
     public set variables(value : Set<Variable>) { this._variables = value; }
 
+    public get testname() : string { return this._testname; }
+    public set testname(value : string) { this._testname = value; }
+    
     public constructor() {
         this.variables = new Set<Variable>();
     }
